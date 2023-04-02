3ac85014dc2eee000d2ce170d57ecb3eea62d01b
Wojtek Pianka <w.pianka@bonasoft.pl>
2023-04-02 17:32:55 +0200

    footer and data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import PageHeader from '@/components/PageHeader.vue'
+import PageFooter from '@/components/PageFooter.vue'
 </script>
 
 <template>
   <main class="container">
     <PageHeader />
     <RouterView />
+    <PageFooter />
   </main>
 </template>
 
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center border-t-4 border-black">
+    <p>&#169; Copyright {{ year }}, MASNET</p>
+  </div>
+</template>
+<script setup lang="ts">
+const year = new Date().getFullYear()
+</script>
+S
